c3e28268a576c6014712f965d8f61c751d4e5532
phamdanghoa <phamdanghoa040500@gmail.com>
2022-01-14 22:04:52 +0700

    add bootstrap 4.0.0

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,22 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require toastr
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import Toastr from "toastr"
 import "channels"
-
+require("bootstrap");
+require("jquery");
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+Toastr.start()
+import 'bootstrap'
+
+
